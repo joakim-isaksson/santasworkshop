9acd285f834d549ac25cb98bc10d853c49af7084
Henri Niva <henri.niva@aalto.fi>
2016-12-05 19:59:22 +0200

    NewtonVR import

---
+++ Assets/NewtonVR/NVRIntegration.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NewtonVR
+{
+    public abstract class NVRIntegration
+    {
+        protected NVRPlayer Player;
+
+        public abstract void Initialize(NVRPlayer player);
+
+        public abstract Vector3 GetPlayspaceBounds();
+    }
+}
---
+++ Assets/NewtonVR/Oculus/NVROculusIntegration.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using UnityEngine;
+
+#if NVR_Oculus
+namespace NewtonVR
+{
+    public class NVROculusIntegration : NVRIntegration
+    {
+        private OVRBoundary boundary;
+        private OVRBoundary Boundary
+        {
+            get
+            {
+                if (boundary == null)
+                {
+                    boundary = new OVRBoundary();
+                }
+                return boundary;
+            }
+        }
+
+        public override void Initialize(NVRPlayer player)
+        {
+            Player = player;
+            Player.gameObject.SetActive(false);
+
+            OVRManager manager = Player.gameObject.AddComponent<OVRManager>();
+            manager.trackingOriginType = OVRManager.TrackingOrigin.FloorLevel;
+
+            OVRCameraRig rig = Player.gameObject.AddComponent<OVRCameraRig>();
+
+            NVRHelpers.SetProperty(rig, "trackingSpace", Player.transform, true);
+            NVRHelpers.SetProperty(rig, "leftHandAnchor", Player.LeftHand.transform, true);
+            NVRHelpers.SetProperty(rig, "rightHandAnchor", Player.RightHand.transform, true);
+            NVRHelpers.SetProperty(rig, "centerEyeAnchor", Player.Head.transform, true);
+
+            Player.gameObject.SetActive(true);
+        }
+
+        private Vector3 PlayspaceBounds = Vector3.zero;
+        public override Vector3 GetPlayspaceBounds()
+        {
+            bool configured = Boundary.GetConfigured();
+            if (configured == true)
+            {
+                PlayspaceBounds = Boundary.GetDimensions(OVRBoundary.BoundaryType.OuterBoundary);
+            }
+
+            return PlayspaceBounds;
+        }
+    }
+}
+#else
+namespace NewtonVR
+{
+    public class NVROculusIntegration : NVRIntegration
+    {
+        public override void Initialize(NVRPlayer player)
+        {
+        }
+
+        public override Vector3 GetPlayspaceBounds()
+        {
+            return Vector3.zero;
+        }
+    }
+}
+#endif
