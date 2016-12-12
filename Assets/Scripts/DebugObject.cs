dd07078e5e02490979b09005f4de2a4f727d9860
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2016-12-12 16:38:57 +0200

    added new skybox and terrain

---
+++ Assets/Scripts/DebugObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugObject : MonoBehaviour {
+
+	void Update () {
+        if (GameManager.instance.DebugMode) gameObject.SetActive(true);
+        else gameObject.SetActive(false);
+    }
+}
