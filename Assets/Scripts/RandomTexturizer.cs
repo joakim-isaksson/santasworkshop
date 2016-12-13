a0c1e55c6dfeb9451be9cc5e415882fa62e1e9ca
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2016-12-13 16:08:36 +0200

    Random textures for toys + some toys on the bookcase

---
+++ Assets/Scripts/RandomTexturizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTexturizer : MonoBehaviour {
+
+	public Texture[] RandomTextures;
+
+	// Use this for initialization
+	void Start () {
+		Texture randomTexture = RandomTextures[Random.Range(0, RandomTextures.Length)];
+
+		GetComponent<MeshRenderer>().material.mainTexture = randomTexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
