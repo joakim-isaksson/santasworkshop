5d42472ebae0a20015201c2a04d565f692ef80cb
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2016-12-14 16:36:01 +0200

    General fixes and adjustments

--- Assets/Scripts/RandomTexturizer.cs
+++ Assets/Scripts/RandomTexturizer.cs
@@ -6,11 +6,25 @@ public class RandomTexturizer : MonoBehaviour {
 
 	public Texture[] RandomTextures;
 
+	[HideInInspector]
+	public int ChosenTexture;
+
+	public GameObject linkedParent;
+
 	// Use this for initialization
-	void Start () {
-		Texture randomTexture = RandomTextures[Random.Range(0, RandomTextures.Length)];
+	void Awake () {
+		ChosenTexture = Random.Range(0, RandomTextures.Length);
+        Texture randomTexture = RandomTextures[ChosenTexture];
 
 		GetComponent<MeshRenderer>().material.mainTexture = randomTexture;
+	}
+
+	void Start()
+	{
+		if (linkedParent != null)
+		{
+			GetComponent<MeshRenderer>().material.mainTexture = RandomTextures[linkedParent.GetComponent<RandomTexturizer>().ChosenTexture];
+		}
 	}
 	
 	// Update is called once per frame
